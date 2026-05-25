1fe526d0c459e003a8be80ae0452b6bb2e32e72a
Apollo-15 <rayl1x.gta@gmail.com>
2026-05-25 13:47:57 +0200

    Removed the unused `<iostream>` include

--- binance_hft/src/main.cpp
+++ binance_hft/src/main.cpp
@@ -1,10 +1,9 @@
-#include <iostream>
 #include "network/websocket_client.hpp"
 
 int main()
 {
     Net::io_context ioContext;
-    Net::ssl::context sslContext{boost::asio::ssl::context::tlsv12_client };
+    Net::ssl::context sslContext{ Net::ssl::context::tlsv12_client };
 
     auto client = std::make_shared<Binance::WebSocketClient>(
         "stream.binance.com",
