040374a03cd24362b199ef43a66a9cce7be5722b
Apollo-15 <rayl1x.gta@gmail.com>
2026-05-26 22:46:28 +0200

    feat: Add `Portfolio` header-only class and integrate it into `main.cpp`
    - Implemented `portfolio.hpp` as a header-only Singleton using Rule of Five
    - Added getters and setters protected with `std::mutex`
    - `TradeQueue` is now passed by reference to `WebSocketClient`
    Added `portfolioThread` in `main.cpp` using a while/yield loop

---
+++ binance_hft/include/portfolio/portfolio.hpp
@@ -0,0 +1,75 @@
+#pragma once
+#include <mutex>
+
+class Portfolio
+{
+private:
+	mutable std::mutex Mutex;
+	double Balance = 50000.0;
+	double PnL = 0.0;
+	double Position = 0.0;
+
+	explicit Portfolio(const double balance)
+		: Balance(balance)
+	{
+	}
+public:
+
+	~Portfolio() = default;
+
+	static Portfolio& Instance(const double balance)
+	{
+		static Portfolio single(balance);
+		return single;
+	}
+
+	Portfolio(const Portfolio&) = delete;
+	Portfolio(Portfolio&&) = delete;
+	Portfolio& operator=(const Portfolio&) = delete;
+	Portfolio& operator=(Portfolio&&) = delete;
+
+
+	[[nodiscard]]
+	double GetBalance() const
+	{
+		std::scoped_lock<std::mutex> lock(Mutex);
+
+		return Balance;
+	}
+	[[nodiscard]]
+	double GetPnL() const
+	{
+		std::scoped_lock<std::mutex> lock(Mutex);
+
+		return PnL;
+	}
+	[[nodiscard]]
+	double GetPosition() const
+	{
+		std::scoped_lock<std::mutex> lock(Mutex);
+
+		return Position;
+	}
+
+
+	void UpdateBalance(const double balance)
+	{
+		std::scoped_lock<std::mutex> lock(Mutex);
+
+		Balance = balance;
+	}
+
+	void UpdatePnL(const double pnl)
+	{
+		std::scoped_lock<std::mutex> lock(Mutex);
+
+		PnL = pnl;
+	}
+
+	void UpdatePosition(const double position)
+	{
+		std::scoped_lock<std::mutex> lock(Mutex);
+
+		Position = position;
+	}
+};
